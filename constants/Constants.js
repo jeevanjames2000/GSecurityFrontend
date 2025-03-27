d4c83e13b06221323a0301b1be0515f31dfca77e
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-03-27 17:32:05 +0530

    updated the api

--- constants/Constants.js
+++ constants/Constants.js
@@ -1,9 +1,6 @@
 const Constants = {
   GET_PROFILE_PASSWORD: "Cats@2025",
   LOGIN_GYM_API_URL: "https://studentmobileapi.gitam.edu/LoginGym",
-  // GSecurity_API_URL: "http://192.168.63.169:9000",
-
-  GSecurity_API_URL: "https://bced-103-23-29-117.ngrok-free.app",
   GSecurity_API_URL: "https://gappapis.gitam.edu/GSecurityBE",
 };
 
